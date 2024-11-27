40d7cca8ee38f8921754775087e638e6d1f0341e
saiVardhan1802 <svchalaka@gmail.com>
2024-11-27 18:53:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movies' element={<Movies />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
